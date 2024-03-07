c07c62a51d28a960625ef13222f2650537fafdd8
Rohan Kumar <96616262+rk4rohankumar@users.noreply.github.com>
2024-03-07 22:53:51 +0530

    navbar fixed

--- delete/src/components/Header Components/GetStarted.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const GetStarted = () => {
-  return (
-    <>Get Started</>
-  )
-}
-
-export default GetStarted
--- delete/src/components/Header Components/ResourceCenter.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ResourceCenter = () => {
-  return (
-    <>Resource Center</>
-  )
-}
-
-export default ResourceCenter
