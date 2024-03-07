b9733ca2fb3712b302fdda92829d78c2e604f677
Rohan Kumar <96616262+rk4rohankumar@users.noreply.github.com>
2024-03-07 23:11:43 +0530

    header fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
         <div className='flex justify-between items-center shadow-xl p-4 lg:p-8'>
             <div className='m-2 lg:m-5'>
                 <img src="https://i.ibb.co/kBjb3Cj/koinx-logo.png" alt="Koinx Logo" className='h-8 lg:h-10' />
             </div>
-            <div className='flex items-center space-x-4 lg:space-x-8 font-semibold'>
-                <Link to="/crypto-taxes" className="text-black hover:text-blue-500">
+            <div className='md:flex md:items-center md:space-x-8 hidden'>
+                <Link to="/crypto-taxes" className="">
                     Crypto Taxes
                 </Link>
-                <Link to="/free-tools" className="text-black hover:text-blue-500">
+                <Link to="/free-tools" className="">
                     Free Tools
                 </Link>
-                <Link to="/resources" className="text-black hover:text-blue-500">
+                <Link to="/resources" className="">
                     Resources
                 </Link>
                 <Link to="/get-started">
@@ -22,6 +28,36 @@ const Header = () => {
                         Get Started
                     </button>
                 </Link>
+            </div>
+            <div className='md:hidden'>
+                <button onClick={toggleMenu} className="text-blue-600 bg-gray-100 py-2 rounded-md text-black px-4 text-lg hover:text-blue-500">
+                <img className="w-[20px]" src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Hamburger_icon.svg/1024px-Hamburger_icon.svg.png" alt=""/>
+                </button>
+                {isOpen && (
+                    <div className="fixed top-0 right-0 m-4 z-50">
+                        <div className="bg-white rounded-md p-4 shadow-md">
+                            <div className="mb-4">
+                                <Link to="/crypto-taxes" className="block py-2 hover:text-blue-500">
+                                    Crypto Taxes
+                                </Link>
+                                <Link to="/free-tools" className="block py-2 hover:text-blue-500">
+                                    Free Tools
+                                </Link>
+                                <Link to="/resources" className="block py-2 hover:text-blue-500">
+                                    Resources
+                                </Link>
+                                <Link to="/get-started" className="block py-2">
+                                    <button className="border px-4 py-2 rounded-md bg-blue-500 text-white w-full">
+                                        Get Started
+                                    </button>
+                                </Link>
+                            </div>
+                            <button onClick={toggleMenu} className="text-blue-600 bg-gray-100 py-2 rounded-md text-black px-4 text-lg hover:text-blue-500">
+                                Close
+                            </button>
+                        </div>
+                    </div>
+                )}
             </div>
         </div>
     );
